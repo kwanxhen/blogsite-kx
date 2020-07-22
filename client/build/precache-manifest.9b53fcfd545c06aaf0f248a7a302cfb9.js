63ef7c334e7e93924c2f19b8f74eab3bc115e043
kwanxhen <kwanxhenng@gmail.com>
2020-07-22 19:56:43 +0800

    modified gitignore file

---
+++ client/build/precache-manifest.9b53fcfd545c06aaf0f248a7a302cfb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ceda46456b77c64b7f5fc66c1994bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "782fc0ae42b3e27da0cf",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "15d16e4e8fa8f7a625ef",
+    "url": "/static/js/2.0d95206f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.0d95206f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "782fc0ae42b3e27da0cf",
+    "url": "/static/js/main.647a1d58.chunk.js"
+  },
+  {
+    "revision": "1fe79edee5f146775588",
+    "url": "/static/js/runtime-main.5575fbcf.js"
+  }
+]);
